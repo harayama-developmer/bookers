e8a61c902ca3eade6db5c8490ea6717a30941b3d
TakuyaHarayama <harayama.develop@gmail.com>
2020-09-08 23:22:50 +0900

    feat: add devise sign_in and sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Devise
   devise_for :users
 
+  resources :users, only: %i[index show edit update]
+  resources :books, only: %i[index show edit create update destroy]
+
   # Static pages
   get 'home' => 'home#index'
   get 'home/about' => 'home#about'
