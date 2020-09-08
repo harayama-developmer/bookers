24151cbc60f70b128bfdf9332eea5a666ff37824
TakuyaHarayama <harayama.develop@gmail.com>
2020-09-09 00:32:45 +0900

    [WIP] style: base layout after sign_in

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# Added
+Refile.secret_key = 'cf02ffcf69ce75576b54c744a8592370b0a159a749160d963404b11ef910997cf49f5692820a0ac5c9a2abd9920a895e6afed1b9a1293f6fbfb65f2da223a0e6'
