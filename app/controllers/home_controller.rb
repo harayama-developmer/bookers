d2b981792078d51428f1c1501e31f352bbff2b82
TakuyaHarayama <harayama.develop@gmail.com>
2020-09-08 13:30:06 +0900

    feat: add about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: %i[index about]
 
-  def index
-  end
+  def index; end
+
+  def about; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # Devise
   devise_for :users
 
+  # Static pages
+  get 'home' => 'home#index'
+  get 'home/about' => 'home#about'
+
   # root
-  root to: "home#index"
+  root to: 'home#index'
 end
